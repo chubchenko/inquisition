a77ed1f863a47b70a6ede7dec10b34798f63b9e1
Maxim Maxim <maxim.koptsov@rubygarage.org>
2019-03-15 20:23:53 +0200

    add header for index page

---
+++ lib/inquisition/core/presenters/rails_about_presenter.rb
@@ -0,0 +1,41 @@
+module Inquisition
+  module Core
+    module Presenters
+      class RailsAboutPresenter < BasePresenter
+        INFO_DATABASE_ADAPTER = 'Database adapter'.freeze
+        COMMAND = 'rake about'.freeze
+
+        def database_adapter
+          prepared_data[INFO_DATABASE_ADAPTER]
+        end
+
+        private
+
+        def parsed_data
+          raw_data.split("\n").map(&method(:split_data)).flatten.reject(&:blank?).tap(&:shift)
+        end
+
+        def split_data(info)
+          info.split('  ')
+        end
+
+        def prepared_data
+          @prepared_data ||= Hash[*parsed_data]
+        end
+
+        def raw_data
+          @raw_data ||= run_cli(COMMAND)
+        end
+
+        def run_cli(command)
+          begin
+            stdout = Open3.capture3(command).first
+          rescue Errno::ENOENT
+            stdout = ''
+          end
+          stdout
+        end
+      end
+    end
+  end
+end
