2da3a8e9f88172ff978bc894fa61a7698e12c6f7
Maxim Maxim <maxim.koptsov@rubygarage.org>
2019-03-21 17:23:13 +0200

    prepare release

--- lib/inquisition/auditors/backend/installer.rb
+++ lib/inquisition/auditors/backend/installer.rb
@@ -8,17 +8,17 @@ module Inquisition
           def installers
             {
               # Bullet::Installer => config_path(:bullet),
-              # Fasterer::Installer => config_path(:fasterer),
-              # Reek::Installer => config_path(:reek),
+              Fasterer::Installer => config_path(:fasterer),
+              Reek::Installer => config_path(:reek),
               # HamlLint::Installer => config_path(:haml_lint),
-              # LolDba::Installer => config_path(:lol_dba),
+              LolDba::Installer => config_path(:lol_dba),
               # RailsErd::Installer => config_path(:rails_erd),
-              # Rubocop::Installer => config_path(:rubocop),
+              Rubocop::Installer => config_path(:rubocop),
               # RubocopRspec::Installer => config_path(:rubocop_rspec),
-              # RubyLint::Installer => config_path(:ruby_lint),
+              RubyLint::Installer => config_path(:ruby_lint),
               # Simplecov::Installer => config_path(:simplecov),
               # SlimLint::Installer => config_path(:slim_lint),
-              # Yamllint::Installer => config_path(:yamllint)
+              Yamllint::Installer => config_path(:yamllint),
               ActiveRecordDoctor::Installer => config_path(:active_record_doctor)
             }
           end
