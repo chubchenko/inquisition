90443997550e8abd9afadb9b9ad2ae8e7cc8cc47
Rostik <31447852+rostikkkk2@users.noreply.github.com>
2019-07-15 09:35:02 +0300

    Remove the division into backend and frontend auditors (#54)

--- spec/inquisition/auditors/frontend/installer_spec.rb
+++ spec/inquisition/auditors/frontend/installer_spec.rb
@@ -1,8 +1,8 @@
-RSpec.describe Inquisition::Auditors::Frontend::Installer do
+RSpec.describe Inquisition::Auditors::Backend::Installer do
   describe '#call' do
-    subject { Inquisition::Auditors::Frontend::Configure.call }
+    subject { Inquisition::Auditors::Backend::Configure.call }
 
-    xit 'run frontend configure' do
+    xit 'run backend configure' do
     end
   end
 end
