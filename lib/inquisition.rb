454d7c4e9a97b80943b23672987c9a3e0f0108ef
Denys Khomiakov <dnkh86@gmail.com>
2019-07-16 18:02:31 +0300

    Set task environment

--- lib/inquisition.rb
+++ lib/inquisition.rb
@@ -3,7 +3,9 @@ require 'inquisition/configuration'
 require 'inquisition/issue'
 require 'inquisition/runner'
 require 'inquisition/version'
+require 'pry'
 
+require 'inquisition/active_record_doctor/runner'
 require 'inquisition/brakeman/runner'
 require 'inquisition/bundler/audit/runner'
 
