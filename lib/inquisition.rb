8867a243d839d4f164f3511b0c21be91d64d191e
Rostik <31447852+rostikkkk2@users.noreply.github.com>
2019-07-19 15:35:31 +0300

    PEO-315 Implement Logger (#59)
    * Implement logger
    
    * Fix wrongs

--- lib/inquisition.rb
+++ lib/inquisition.rb
@@ -3,6 +3,7 @@ require 'inquisition/configuration'
 require 'inquisition/issue'
 require 'inquisition/runner'
 require 'inquisition/version'
+require 'inquisition/logger'
 
 require 'inquisition/brakeman/runner'
 require 'inquisition/bundler/audit/runner'
