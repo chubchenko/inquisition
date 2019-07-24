55328e0e15de53955c7589c685c0a4cb1d71e06e
rostikkkk2 <rost4402255@gmail.com>
2019-07-24 19:15:26 +0300

    Add the ability to turn on/off a runner

--- lib/inquisition/configuration.rb
+++ lib/inquisition/configuration.rb
@@ -5,11 +5,12 @@ module Inquisition
     include Singleton
 
     CONFIG_FILE_NAME = '.inquisition.yml'.freeze
+    FOLDER_NAME_CONFIG = 'lib/inquisition/default_config'.freeze
     ROOT_PATH = Dir.pwd
-    DEFAULT_HASH = { 'plugins' => [], 'verbose' => false }.freeze
+    # DEFAULT_HASH = { 'plugins' => [], 'verbose' => false }.freeze
 
     def to_h
-      config_exist? ? data_config.to_h : DEFAULT_HASH
+      config_exist? ? data_config.to_h : create_default_config
     end
 
     def verbose?
@@ -24,6 +25,14 @@ module Inquisition
 
     def data_config
       YAML.load_file(File.join(ROOT_PATH, CONFIG_FILE_NAME))
+    end
+
+    def data_default_config
+      YAML.load_file(File.join(ROOT_PATH, FOLDER_NAME_CONFIG, CONFIG_FILE_NAME))
+    end
+
+    def create_default_config
+      File.open("", 'w')
     end
   end
 end
