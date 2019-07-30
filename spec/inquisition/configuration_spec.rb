4e39753eaa5d18b32365c048b00cbe2db4aeea3a
chubchenko <artem.chubchenko@gmail.com>
2019-07-30 10:53:00 +0300

    Update Configuration class & specs

--- spec/inquisition/configuration_spec.rb
+++ spec/inquisition/configuration_spec.rb
@@ -1,45 +1,52 @@
-require 'yaml'
-
 RSpec.describe Inquisition::Configuration do
-  subject { described_class.instance }
-
-  let(:config_file) { Inquisition::Configuration::CONFIG_FILE_NAME }
-  let(:user_configure_file) { { 'plugins' => [], 'verbose' => true } }
-  let(:default_hash) { Inquisition::Configuration::DEFAULT_HASH }
+  subject(:configuration) { Class.new(described_class).instance }
 
   describe '#to_h' do
-    context 'when config file exists' do
+    context 'when the file is missing' do
       before do
-        File.open("./spec/fixtures/#{config_file}", 'w') { |f| f.write user_configure_file.to_yaml }
+        allow(File).to receive(:exist?).with('.inquisition.yml').and_return(false)
       end
 
-      after { File.delete("./spec/fixtures/#{config_file}") }
-
-      it 'file exist' do
-        allow(subject).to receive(:config_exist?).and_return(true)
-        allow(subject).to receive(:data_config).and_return(user_configure_file)
-        expect(subject.to_h).to eq(user_configure_file)
-      end
+      it { expect(configuration.to_h).to eq({}) }
     end
 
-    context 'when config file not exists' do
-      it 'file not exist' do
-        expect(subject.to_h).to eq(default_hash)
+    context 'when file is present' do
+      before do
+        allow(File).to receive(:exist?).with('.inquisition.yml').and_return(true)
+        allow(YAML).to receive(:load_file).and_return(
+          'plugins' => { 'reek' => { 'enabled' => true } }, 'verbose' => false
+        )
       end
+
+      it { expect(configuration.to_h).to eq('plugins' => { 'reek' => { 'enabled' => true } }, 'verbose' => false) }
     end
   end
 
   describe '#verbose?' do
-    context 'when check verbose true or false' do
-      it 'return false' do
-        allow(subject).to receive(:to_h).and_return(default_hash)
-        expect(subject.verbose?).to eq(false)
+    context 'when the file is missing' do
+      before do
+        allow(File).to receive(:exist?).with('.inquisition.yml').and_return(false)
       end
 
-      it 'return true' do
-        allow(subject).to receive(:to_h).and_return(user_configure_file)
-        expect(subject.verbose?).to eq(true)
+      it { is_expected.not_to be_verbose }
+    end
+
+    context 'when a value set to false' do
+      before do
+        allow(File).to receive(:exist?).with('.inquisition.yml').and_return(true)
+        allow(YAML).to receive(:load_file).and_return('verbose' => false)
       end
+
+      it { is_expected.not_to be_verbose }
+    end
+
+    context 'when a value set to true' do
+      before do
+        allow(File).to receive(:exist?).with('.inquisition.yml').and_return(true)
+        allow(YAML).to receive(:load_file).and_return('verbose' => true)
+      end
+
+      it { is_expected.to be_verbose }
     end
   end
 end
