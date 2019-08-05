22422bbf9a5b4eede94842c16adada6bc6c2464a
chubchenko <artem.chubchenko@gmail.com>
2019-08-05 19:39:12 +0300

    Update Brakeman executor

--- lib/inquisition/brakeman/runner.rb
+++ lib/inquisition/brakeman/runner.rb
@@ -1,16 +1,29 @@
 require 'brakeman'
+require 'brakeman/scanner'
 
 module Inquisition
   module Brakeman
-    LEVELS = Hash.new(:low).update(0 => :high, 1 => :medium)
-
     class Runner < ::Inquisition::Runner
+      MATCH_TABLE = Hash.new(
+        Inquisition::Severity::LOW
+      ).update(
+        0 => Inquisition::Severity::HIGH, 1 => Inquisition::Severity::MEDIUM
+      ).freeze
+
       def call
-        ::Brakeman.run('.').warnings.each do |warning|
-          @issues << Issue.new(severity: LEVELS[warning.confidence], path: warning.file.relative,
-                               line: warning.line, message: warning.message.to_s, runner: self)
-        end
-        @issues
+        ::Brakeman.run(app_path: '.').warnings.map(&method(:issue_for))
+      end
+
+      private
+
+      def issue_for(warning)
+        Issue.new(
+          severity: MATCH_TABLE[warning.confidence],
+          path: warning.relative_path,
+          line: warning.line,
+          message: warning.message.to_s,
+          runner: self
+        )
       end
     end
   end
--- spec/inquisition/brakeman/runner_spec.rb
+++ spec/inquisition/brakeman/runner_spec.rb
@@ -1,41 +1,52 @@
-require 'brakeman/scanner'
-
 RSpec.describe Inquisition::Brakeman::Runner do
-  let(:runner) { described_class.new }
-  let(:major_warning)  { instance_double(Brakeman::Warning, confidence: 0, line: nil, file: nil, message: nil) }
-  let(:medium_warning) { instance_double(Brakeman::Warning, confidence: 1, line: nil, file: nil, message: nil) }
-  let(:minor_warning)  { instance_double(Brakeman::Warning, confidence: 2, line: nil, file: nil, message: nil) }
-  let(:warnings) { [major_warning, medium_warning, minor_warning] }
-
-  let(:high_severity_issue) do
-    Inquisition::Issue.new(severity: :high, path: nil, line: nil, message: nil, runner: nil)
-  end
-
-  let(:medium_severity_issue) do
-    Inquisition::Issue.new(severity: :medium, path: nil, line: nil, message: nil, runner: nil)
-  end
-
-  let(:low_severity_issue) do
-    Inquisition::Issue.new(severity: :low, path: nil, line: nil, message: nil, runner: nil)
-  end
-
-  let(:issues) { [high_severity_issue, medium_severity_issue, low_severity_issue] }
+  include_examples 'enablable', 'brakeman'
 
   describe '#call' do
-    before do
-      tracker = instance_double(Brakeman::Tracker)
-      allow(Brakeman).to receive(:run).and_return(tracker)
-      allow(tracker).to receive(:warnings).and_return(warnings)
-      allow(major_warning.file).to receive(:relative)
+    subject(:runner) { described_class.new }
+
+    let(:message) do
+      instance_double(Brakeman::Messages::Message, to_s: 'Potentially dangerous key allowed for mass assignment')
+    end
+    let(:warning) do
+      instance_double(
+        Brakeman::Warning,
+        confidence: confidence,
+        line: 42,
+        relative_path: 'app/controllers/users_controller.rb',
+        message: message
+      )
+    end
+    let(:issue) do
+      Inquisition::Issue.new(
+        path: warning.relative_path, line: warning.line, severity: severity, message: message.to_s, runner: nil
+      )
     end
 
-    it 'returns array with issues' do
-      runner.call.each_with_index do |issue, index|
-        # TODO: will be fixed in the next PR
-        # expect(issue.instance_variable_get(:@severity)).to eq(issues[index].instance_variable_get(:@severity))
-      end
+    before do
+      allow(Brakeman).to receive(:run)
+        .with(app_path: '.')
+        .and_return(instance_double(Brakeman::Tracker, warnings: [warning]))
+    end
+
+    context 'when warning confidence is 0' do
+      let(:confidence) { 0 }
+      let(:severity) { Inquisition::Severity::HIGH }
+
+      it { expect(runner.call).to contain_exactly(issue) }
+    end
+
+    context 'when warning confidence is 1' do
+      let(:confidence) { 1 }
+      let(:severity) { Inquisition::Severity::MEDIUM }
+
+      it { expect(runner.call).to contain_exactly(issue) }
+    end
+
+    context 'when warning confidence is 2' do
+      let(:confidence) { 2 }
+      let(:severity) { Inquisition::Severity::LOW }
+
+      it { expect(runner.call).to contain_exactly(issue) }
     end
   end
-
-  include_examples 'enablable', 'brakeman'
 end
