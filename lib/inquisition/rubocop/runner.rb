c1fdd78fbb4e44a7c75ebbc810b7a8c5042b0e9c
Denys Khomiakov <dnkh86@gmail.com>
2019-08-07 20:07:38 +0300

    Temporary commit [ci skip]

--- lib/inquisition/rubocop/runner.rb
+++ lib/inquisition/rubocop/runner.rb
@@ -12,18 +12,36 @@ module Inquisition
       }.freeze
 
       def call
-        offenses = RuboCopModifiedRunner.new({}, Configuration.call).run(['.'])
-        offenses.each { |offense| create_issue(offense) }
-        @issues
+        issues = RuboCopModifiedRunner.new({}, Configuration.call).run(['.'])
+        issues.map do |path, offense|
+          binding.pry
+          # issues_for(issue)
+        end
+        # offenses.each { |offense| issues_for(offense) }
+        # @issues
       end
 
       private
 
-      def create_issue(offenses)
-        offenses.values.flatten.each do |offense|
-          @issues << Inquisition::Issue.new(severity: LEVELS[offense.severity.name],
-                                            path: offenses.keys[0], message: offense.message,
-                                            runner: self, line: offense.line)
+      def issues_for(offenses1)
+        # offenses.values.flatten.each do |offense|
+        #   @issues << Inquisition::Issue.new(
+        #     severity: LEVELS[offense.severity.name],
+        #     path: offenses.keys[0],
+        #     message: offense.message,
+        #     runner: self,
+        #     line: offense.line)
+        # end
+        offenses1.each do |path, offenses|
+          offenses.each do |offense|
+            @issues << Issue.new(
+              severity: LEVELS[offense.severity.name],
+              path: path,
+              message: offense.message,
+              line: offense.line,
+              runner: self
+            )
+          end
         end
       end
     end
