a989e457b79c0b9ee297d176bf464b3ef5c95cd9
rostikkkk2 <rost4402255@gmail.com>
2019-08-13 13:17:47 +0300

    Add custom config to rails_best_practices runner if user do not have

--- lib/inquisition/rails_best_practices/runner.rb
+++ lib/inquisition/rails_best_practices/runner.rb
@@ -3,10 +3,23 @@ require 'rails_best_practices'
 module Inquisition
   module RailsBestPractices
     class Runner < ::Inquisition::Runner
+      USER_CONFIG_PATH = 'rails_best_practices.yml'.freeze
+      CUSTOM_CONFIG_PATH = File.join(File.dirname(__FILE__), '../../../config/rails_best_practices/config.yml')
+
       def call
         analyzer = ::RailsBestPractices::Analyzer.new(Rails.root, 'silent' => true)
         check_errors(analyzer)
         @issues
+      end
+
+      private
+
+      def options
+        { 'config' => choose_config_path, 'silent' => true }
+      end
+
+      def choose_config_path
+        File.exist?(USER_CONFIG_PATH) ? USER_CONFIG_PATH : CUSTOM_CONFIG_PATH
       end
 
       def check_errors(analyzer)
