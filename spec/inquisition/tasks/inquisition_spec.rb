de5b8d4a2219ffa7d2e0a8d473999bbe1282161a
Denys Khomiakov <dnkh86@gmail.com>
2019-08-14 10:05:15 +0300

    Fix rake spec

--- spec/inquisition/tasks/inquisition_spec.rb
+++ spec/inquisition/tasks/inquisition_spec.rb
@@ -1,14 +1,22 @@
 RSpec.describe 'inquisition:run' do
   let(:collector) { instance_double(Inquisition::Collector) }
+  let(:formatter) { instance_double(Inquisition::Formatters::HtmlFormatter) }
+  let(:collection) { [instance_double(Inquisition::Issue)] }
 
   before do
-    allow(collector).to receive(:call)
+    allow(collector).to receive(:call).and_return(collection)
     allow(Inquisition::Collector).to receive(:new).and_return(collector)
+    allow(formatter).to receive(:call)
+    allow(Inquisition::Formatters::HtmlFormatter).to receive(:new).with(collection).and_return(formatter)
   end
 
   include_context 'rake' do
     before { task.invoke }
 
-    it { expect(collector).to have_received(:call).once }
+    it do
+      expect(collector).to have_received(:call).once
+      expect(Inquisition::Formatters::HtmlFormatter).to have_received(:new).with(collection)
+      expect(formatter).to have_received(:call).once
+    end
   end
 end
