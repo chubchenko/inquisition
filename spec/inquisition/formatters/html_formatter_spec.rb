e11cb8975df5859cb3a615e39e3706bd776be502
Denys Khomiakov <dnkh86@gmail.com>
2019-08-16 17:35:25 +0300

    Divide overview template

--- spec/inquisition/formatters/html_formatter_spec.rb
+++ spec/inquisition/formatters/html_formatter_spec.rb
@@ -1,11 +1,10 @@
 RSpec.describe Inquisition::Formatters::HtmlFormatter do
   describe '#call' do
-    let(:issue) { instance_double(Inquisition::Issue) }
+    let(:runner) { Inquisition::Runner.new }
+    let(:issue) { instance_double(Inquisition::Issue, path: '', line: '', severity: :low, message: '', runner: runner) }
     let(:formatter) { described_class.new([issue]) }
     let(:report_directory) { "#{Rails.root}/inquisition_report" }
     let(:report_content) { ['issues_list.html', 'overview.html', 'assets/.keep'] }
-
-    before { allow(File).to receive(:read).and_return('') }
 
     after { FileUtils.rm_r(report_directory) }
 
