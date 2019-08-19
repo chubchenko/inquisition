a3b9f2e5cc792489474d18e933f0f97ad0e2a708
Denys Khomiakov <dnkh86@gmail.com>
2019-08-19 18:11:35 +0300

    Redo breakdowns builder

--- lib/inquisition/formatters/html/overview/breakdowns_builder.rb
+++ lib/inquisition/formatters/html/overview/breakdowns_builder.rb
@@ -5,12 +5,6 @@ module Inquisition
         class BreakdownsBuilder < HtmlBuilder
           def initialize(collection)
             super
-            @security = []
-            @style = []
-            @performance = []
-            @compatibility = []
-            @unused_code = []
-            @duplication = []
             sort_by_categories
           end
 
@@ -22,12 +16,12 @@ module Inquisition
             <<~JS
               <script type="text/javascript">
                 var category_percentage = [
-                  #{percentage(@security)},
-                  #{percentage(@style)},
-                  #{percentage(@performance)},
-                  #{percentage(@compatibility)},
-                  #{percentage(@unused_code)},
-                  #{percentage(@duplication)}
+                  #{percentage(@categorized_issues[:security])},
+                  #{percentage(@categorized_issues[:style])},
+                  #{percentage(@categorized_issues[:performance])},
+                  #{percentage(@categorized_issues[:compatibility])},
+                  #{percentage(@categorized_issues[:unused_code])},
+                  #{percentage(@categorized_issues[:duplication])}
                 ];
               </script>
             JS
@@ -36,21 +30,13 @@ module Inquisition
           private
 
           def sort_by_categories
-            @collection.each do |issue|
-              case issue # remove line 23 & uncomment 24 when categories will be added
-              # case issue.category
-              when :security then @security << issue
-              when :style then @style << issue
-              when :performance then @performance << issue
-              when :compatibility then @compatibility << issue
-              when :unused_code then @unused_code << issue
-              when :duplication then @duplication << issue
-              end
-            end
+            @categorized_issues = { security: [], style: [], performance: [], compatibility: [],
+                                    unused_code: [], duplication: [] } # remove when category will be added
+
+            # @categorized_issues = @collection.group_by(&:category)
           end
 
           def percentage(category)
-            100 / 6 # remove line 36 & uncomment 37 when categories will be added
             # category.count * 100 / @collection.count
           end
         end
