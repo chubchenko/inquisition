da67329cf3e97ab4ac048f06554567d3d4f1c0ec
Denys Khomiakov <dnkh86@gmail.com>
2019-08-19 17:16:27 +0300

    Shift to private some breakdown builder methods

--- lib/inquisition/formatters/html/overview/breakdowns_builder.rb
+++ lib/inquisition/formatters/html/overview/breakdowns_builder.rb
@@ -18,6 +18,23 @@ module Inquisition
             'breakdowns.html'
           end
 
+          def js_chart
+            <<~JS
+              <script type="text/javascript">
+                var category_percentage = [
+                  #{percentage(@security)},
+                  #{percentage(@style)},
+                  #{percentage(@performance)},
+                  #{percentage(@compatibility)},
+                  #{percentage(@unused_code)},
+                  #{percentage(@duplication)}
+                ];
+              </script>
+            JS
+          end
+
+          private
+
           def sort_by_categories
             @collection.each do |issue|
               case issue # remove line 23 & uncomment 24 when categories will be added
@@ -35,21 +52,6 @@ module Inquisition
           def percentage(category)
             100 / 6 # remove line 36 & uncomment 37 when categories will be added
             # category.count * 100 / @collection.count
-          end
-
-          def js_chart
-            <<~JS
-              <script type="text/javascript">
-                var category_percentage = [
-                  #{percentage(@security)},
-                  #{percentage(@style)},
-                  #{percentage(@performance)},
-                  #{percentage(@compatibility)},
-                  #{percentage(@unused_code)},
-                  #{percentage(@duplication)}
-                ];
-              </script>
-            JS
           end
         end
       end
