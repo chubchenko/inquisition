9ba871310fe4e541d7795852d7c41ac27c4ac758
Denys Khomiakov <dnkh86@gmail.com>
2019-08-22 11:00:57 +0300

    Minor fixes

--- spec/inquisition/formatters/html_formatter_spec.rb
+++ spec/inquisition/formatters/html_formatter_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Inquisition::Formatters::HtmlFormatter do
     let(:runner) { Inquisition::Runner.new }
     let(:issue) { instance_double(Inquisition::Issue, path: '', line: '', severity: :low, message: '', runner: runner) }
     let(:formatter) { described_class.new([issue]) }
-    let(:report_directory) { "#{Rails.root}/inquisition" }
+    let(:report_directory) { File.join(Rails.root, 'inquisition') }
     let(:report_content) { ['issues_list.html', 'overview.html', 'assets/.keep'] }
 
     after { FileUtils.rm_r(report_directory) }
