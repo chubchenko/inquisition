86896bfcaffe2c23230f2fa0937a3fa5c4c4aed7
Denys Khomiakov <dnkh86@gmail.com>
2019-08-23 13:16:12 +0300

    Add category to Traceroute

--- spec/inquisition/traceroute/runner_spec.rb
+++ spec/inquisition/traceroute/runner_spec.rb
@@ -5,25 +5,26 @@ RSpec.describe Inquisition::Traceroute::Runner do
   let(:unreachable_action) { 'bar#baz' }
 
   let(:unused_route_issue) do
-    { severity: :low, message: "unused route: #{unused_route}" }
+    { category: :unused_code, severity: :low, message: "unused route: #{unused_route}" }
   end
 
   let(:unreachable_action_issue) do
-    { severity: :low, message: "unreachable action method: #{unreachable_action}" }
+    { category: :unused_code, severity: :low, message: "unreachable action method: #{unreachable_action}" }
   end
 
   let(:issues) { [unused_route_issue, unreachable_action_issue] }
 
+  before do
+    allow(runner).to receive(:unused_routes).and_return([unused_route])
+    allow(runner).to receive(:unreachable_action_methods).and_return([unreachable_action])
+  end
+
   describe '#call' do
     it 'returns array with issues' do
-      expect(Traceroute).to receive(:new).and_return(traceroute)
-      expect(traceroute).to receive(:load_everything!)
-      expect(runner).to receive(:unused_routes).and_return([unused_route])
-      expect(runner).to receive(:unreachable_action_methods).and_return([unreachable_action])
       runner.call.each_with_index do |issue, index|
-        # TODO: will be fixed in the next PR
-        # expect(issue.instance_variable_get(:@severity)).to eq(issues[index][:severity])
-        expect(issue.instance_variable_get(:@message)).to eq(issues[index][:message])
+        expect(issue.message).to eq(issues[index][:message])
+        expect(issue.severity.name).to eq(issues[index][:severity])
+        expect(issue.category.name).to eq(issues[index][:category])
       end
     end
   end
