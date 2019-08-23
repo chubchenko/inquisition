627ae69918445a63b56c6c2f499405c74c770381
Denys Khomiakov <dnkh86@gmail.com>
2019-08-23 17:27:28 +0300

    PEO-465 Fasterer Categorization (#96)

--- spec/inquisition/fasterer/runner_spec.rb
+++ spec/inquisition/fasterer/runner_spec.rb
@@ -6,9 +6,12 @@ RSpec.describe Inquisition::Fasterer::Runner do
 
     let(:test_file) { 'app/controllers/application_controller.rb' }
     let(:instance_file_traverser) { instance_double(Inquisition::Fasterer::FileTraverser) }
-    let(:instance_analyzer) { instance_double(Fasterer::Analyzer, file_path: "#{Rails.root}/#{test_file}", errors: offense_collector) }
     let(:offense_collector) { instance_double('Fasterer::Analyzer::OffenseCollector', offenses: offense) }
     let(:offense) { [instance_double(Fasterer::Offense, explanation: 'error', line_number: 1)] }
+
+    let(:instance_analyzer) do
+      instance_double(Fasterer::Analyzer, file_path: "#{Rails.root}/#{test_file}", errors: offense_collector)
+    end
 
     before do
       allow(Inquisition::Fasterer::FileTraverser).to receive(:new).and_return(instance_file_traverser)
@@ -20,18 +23,20 @@ RSpec.describe Inquisition::Fasterer::Runner do
     context 'when call with errors' do
       before do
         allow(instance_analyzer).to receive(:errors).and_return(offense)
-        allow(instance_file_traverser).to receive(:offenses_grouped_by_type).and_return({ error: offense_collector })
+        allow(instance_file_traverser).to receive(:offenses_grouped_by_type).and_return(error: offense_collector)
       end
 
       it 'returns a collection of issues' do
-        expect(runner_result).to contain_exactly(Inquisition::Issue.new(
-          severity: :low,
-          line: offense.first.line_number,
-          path: test_file,
-          message: offense.first.explanation,
-          runner: nil
+        expect(runner_result).to contain_exactly(
+          Inquisition::Issue.new(
+            severity: :low,
+            category: :perfomance,
+            line: offense.first.line_number,
+            path: test_file,
+            message: offense.first.explanation,
+            runner: nil
+          )
         )
-      )
       end
     end
 
