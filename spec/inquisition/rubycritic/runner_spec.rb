7cf018b664c8a406368e0006d53fb7ec25ec389d
Denys Khomiakov <dnkh86@gmail.com>
2019-08-27 09:44:21 +0300

    PEO-469 Rubycritic Categorization (#100)
    * Add category to ruby critic
    
    * Fix ruby critic runner spec

--- spec/inquisition/rubycritic/runner_spec.rb
+++ spec/inquisition/rubycritic/runner_spec.rb
@@ -2,43 +2,55 @@ RSpec.describe Inquisition::Rubycritic::Runner do
   include_examples 'enablable', 'rubycritic'
 
   describe '#call' do
-    subject(:result_runner) { described_class.new.call }
+    subject(:runner_result) { described_class.new.call }
 
-    let(:analyse_module_without_errors) { [instance_double('RubyCritic::AnalysedModule', smells: [])] }
     let(:analyser_reek) { instance_double(Inquisition::Rubycritic::Analysers::Reek) }
     let(:analyser_flay) { instance_double(Inquisition::Rubycritic::Analysers::Flay) }
     let(:analyser_flog) { instance_double(Inquisition::Rubycritic::Analysers::Flog) }
-    let(:smell) { instance_double('RubyCritic::Smell', locations: [smell_location], message: 'test message') }
-    let(:smell_location) { instance_double('RubyCritic::Location', line: 1, pathname: pathname) }
-    let(:pathname) do
-      Rails.root + 'app/controllers/application_controller.rb'
-    end
+
+    let(:smell) { instance_double(RubyCritic::Smell, locations: [smell_location], message: 'test message') }
+    let(:smell_location) { instance_double(RubyCritic::Location, line: 1, pathname: Rails.root + pathname) }
+    let(:pathname) { 'app/controllers/application_controller.rb' }
 
     before do
       allow(Inquisition::Rubycritic::Analysers::Reek).to receive(:new).and_return(analyser_reek)
       allow(Inquisition::Rubycritic::Analysers::Flay).to receive(:new).and_return(analyser_flay)
       allow(Inquisition::Rubycritic::Analysers::Flog).to receive(:new).and_return(analyser_flog)
-      allow(analyser_reek).to receive(:run).and_return(analyse_module)
-      allow(analyser_flay).to receive(:run).and_return(analyse_module_without_errors)
-      allow(analyser_flog).to receive(:run).and_return(analyse_module_without_errors)
-    end
 
-    context 'when runner returns errors' do
-      let(:analyse_module) { [instance_double('RubyCritic::AnalysedModule', smells: [smell])] }
-
-      it 'returns a collection of issues' do
-        expect(result_runner).to contain_exactly(Inquisition::Issue.new(
-          severity: :low,
-          path: 'app/controllers/application_controller.rb',
-          line: smell_location.line,
-          runner: nil,
-          message: smell.message
-        ))
+      [analyser_reek, analyser_flay, analyser_flog].each do |analyser|
+        allow(analyser).to receive(:run).and_return(analysed_module)
       end
     end
 
+    context 'when runner returns errors' do
+      let(:analysed_module) { [instance_double(RubyCritic::AnalysedModule, smells: [smell])] }
+      let(:duplication_issue) do
+        Inquisition::Issue.new(
+          severity: :low,
+          category: :duplication,
+          path: pathname,
+          line: smell_location.line,
+          runner: nil,
+          message: smell.message
+        )
+      end
+
+      let(:complexity_issue) do
+        Inquisition::Issue.new(
+          severity: :low,
+          category: :complexity,
+          path: pathname,
+          line: smell_location.line,
+          runner: nil,
+          message: smell.message
+        )
+      end
+
+      it { is_expected.to match_array([duplication_issue, complexity_issue, complexity_issue]) }
+    end
+
     context 'when runner success' do
-      let(:analyse_module) { [instance_double('RubyCritic::AnalysedModule', smells: [])] }
+      let(:analysed_module) { [instance_double(RubyCritic::AnalysedModule, smells: [])] }
 
       it { is_expected.to be_empty }
     end
