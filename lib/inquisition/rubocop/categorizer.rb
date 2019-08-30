592102d2aaf6f6113bac1408506238b8e90dbedf
Denys Khomiakov <dnkh86@gmail.com>
2019-08-30 15:27:46 +0300

    Fix codeclimate issue

--- lib/inquisition/rubocop/categorizer.rb
+++ lib/inquisition/rubocop/categorizer.rb
@@ -1,22 +1,24 @@
 module Inquisition
   module Rubocop
     class Categorizer
-      DETAILED_MATCH_TABLE = {
+      SPECIFIC_MATCH_TABLE = {
         'Bundler/DuplicatedGem' => :duplication,
         'Bundler/InsecureProtocolSource' => :security,
         'Metrics/LineLength' => :style,
         'Migrations/RemoveIndex' => :performance
       }.freeze
 
-      MATCH_TABLE = {
+      COMMON_MATCH_TABLE = {
         'Security' => :security,
         'Metrics' => :complexity,
         'Performance' => :performance
       }.freeze
 
       def self.find_category(cop_name)
-        return DETAILED_MATCH_TABLE[cop_name] if DETAILED_MATCH_TABLE.key?(cop_name)
-        return MATCH_TABLE[cop_name.split('/')[0]] if MATCH_TABLE.key?(cop_name.split('/')[0])
+        return SPECIFIC_MATCH_TABLE[cop_name] if SPECIFIC_MATCH_TABLE.key?(cop_name)
+
+        cop_module = cop_name.split('/')[0]
+        return COMMON_MATCH_TABLE[cop_module] if COMMON_MATCH_TABLE.key?(cop_module)
 
         :style
       end
