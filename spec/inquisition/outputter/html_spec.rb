ae36024eed3a88746f5b11c6681200f7ed4ae553
Denys Khomiakov <dnkh86@gmail.com>
2019-09-02 16:56:35 +0300

    Add html outputter spec

--- spec/inquisition/outputter/html_spec.rb
+++ spec/inquisition/outputter/html_spec.rb
@@ -1,5 +1,18 @@
 RSpec.describe Inquisition::Outputter::HTML do
+  let(:formatter) { instance_double(Inquisition::Formatters::HtmlFormatter) }
+  let(:html) { described_class.new }
+
   describe '#stop' do
-    it
+    before do
+      allow(formatter).to receive(:call)
+      allow(Inquisition::Formatters::HtmlFormatter).to receive(:new).and_return(formatter)
+      Inquisition::Configuration.instance.loader.add(html)
+      Inquisition::Configuration.instance.fanout.around
+    end
+
+    it 'calls instance of html formatter' do
+      expect(Inquisition::Formatters::HtmlFormatter).to have_received(:new)
+      expect(formatter).to have_received(:call)
+    end
   end
 end
