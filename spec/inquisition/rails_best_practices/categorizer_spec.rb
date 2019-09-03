36fcf9ba896622fb01baaf7e79960c9693341dad
Denys Khomiakov <dnkh86@gmail.com>
2019-09-03 14:57:09 +0300

    Add requested changes

--- spec/inquisition/rails_best_practices/categorizer_spec.rb
+++ spec/inquisition/rails_best_practices/categorizer_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Inquisition::RailsBestPractices::Categorizer do
-  describe '#find_category' do
-    %i[complexity style bug_risk unused_code performance security duplication].each do |issue_category|
+  describe '.find_category' do
+    Inquisition::Category::NAMES.each do |issue_category|
       it "returns #{issue_category} issue category for specific error type" do
         Inquisition::RailsBestPractices::Categorizer::MATCH_TABLE[issue_category].each do |error_type|
           expect(described_class.find_category(error_type)).to eq(issue_category)
--- spec/inquisition/rails_best_practices/issue_spec.rb
+++ spec/inquisition/rails_best_practices/issue_spec.rb
@@ -2,9 +2,9 @@ RSpec.describe Inquisition::RailsBestPractices::Issue do
   describe '#to_h' do
     subject(:issue) { described_class.new(error) }
 
-    let(:message) { 'test message' }
-    let(:path) { 'test path' }
-    let(:line) { '1' }
+    let(:message) { 'not use time_ago_in_words' }
+    let(:path) { 'app/views/layouts/application.html.erb' }
+    let(:line) { 13 }
 
     let(:error) do
       instance_double(
@@ -12,11 +12,11 @@ RSpec.describe Inquisition::RailsBestPractices::Issue do
         short_filename: path,
         line_number: line,
         message: message,
-        type: 'RailsBestPractices::Reviews::AddModelVirtualAttributeReview'
+        type: 'RailsBestPractices::Reviews::NotUseTimeAgoInWordsReview'
       )
     end
 
-    let(:options) { { category: :complexity, severity: :low, message: message, path: path, line: line.to_i } }
+    let(:options) { { category: :style, severity: :low, message: message, path: path, line: line.to_i } }
 
     it { expect(issue.to_h).to include(options) }
   end
