a706bce6523c13c8cfafa26895c3d7da19ba358a
Denys Khomiakov <dnkh86@gmail.com>
2019-09-03 08:54:49 +0300

    Fix rubocop categorizer spec

--- spec/inquisition/rubocop/categorizer_spec.rb
+++ spec/inquisition/rubocop/categorizer_spec.rb
@@ -1,38 +1,59 @@
 RSpec.describe Inquisition::Rubocop::Categorizer do
-  describe '#find_category' do
+  describe '.find_category' do
     subject { described_class.find_category(cop_name) == category }
 
-    context 'when offence category is security' do
-      let(:cop_name) { 'Security/Eval' }
-      let(:category) { Inquisition::Category::SECURITY }
-
-      it { is_expected.to be_truthy }
-    end
-
-    context 'when offence category is duplication' do
+    context 'when cop name is Bundler/DuplicatedGem' do
       let(:cop_name) { 'Bundler/DuplicatedGem' }
-      let(:category) { Inquisition::Category::DUPLICATION }
+      let(:category) { :duplication }
 
       it { is_expected.to be_truthy }
     end
 
-    context 'when offence category is style' do
+    context 'when cop name is Bundler/InsecureProtocolSource' do
+      let(:cop_name) { 'Bundler/InsecureProtocolSource' }
+      let(:category) { :security }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when cop name is Metrics/LineLength' do
       let(:cop_name) { 'Metrics/LineLength' }
-      let(:category) { Inquisition::Category::STYLE }
+      let(:category) { :style }
 
       it { is_expected.to be_truthy }
     end
 
-    context 'when offence category is performance' do
+    context 'when cop name is Migrations/RemoveIndex' do
       let(:cop_name) { 'Migrations/RemoveIndex' }
-      let(:category) { Inquisition::Category::PERFORMANCE }
+      let(:category) { :performance }
 
       it { is_expected.to be_truthy }
     end
 
-    context 'when offence category is complexity' do
-      let(:cop_name) { 'Metrics/BlockLength' }
-      let(:category) { Inquisition::Category::COMPLEXITY }
+    context 'when cop name is Security' do
+      let(:cop_name) { 'Security' }
+      let(:category) { :security }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when cop name is Metrics' do
+      let(:cop_name) { 'Metrics' }
+      let(:category) { :complexity }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when cop name is Performance' do
+      let(:cop_name) { 'Performance' }
+      let(:category) { :performance }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when cop name doesnt belong to any category from match tables' do
+      let(:cop_name) { 'Foo/Bar' }
+      let(:category) { :style }
 
       it { is_expected.to be_truthy }
     end
