fd87b2456632f73164c7148009b52f7f9ff578b0
Denys Khomiakov <dnkh86@gmail.com>
2019-09-05 18:23:56 +0300

    Bundler leak refactoring

--- lib/inquisition/bundler/leak/runner.rb
+++ lib/inquisition/bundler/leak/runner.rb
@@ -5,15 +5,12 @@ module Inquisition
     module Leak
       class Runner < ::Inquisition::Runner
         def call
-          ::Bundler::Plumber::Scanner.new.scan do |result|
-            @issues << Issue.new(
-              severity: :low,
-              path: 'Gemfile.lock',
-              line: nil,
-              message: "#{result.gem.name} #{result.gem.version} - #{result.advisory.title}",
-              runner: self
-            )
-          end
+          ::Bundler::Plumber::Scanner.new.scan { |warning| issue_for(warning) }
+          @issues
+        end
+
+        def issue_for(warning)
+          @issues << Issue.new(Vulnerability.new(warning).to_h.merge(runner: self))
         end
       end
     end
