32f233a8a639b9a87b830ff1421338afed95f8c9
rostikkkk2 <rost4402255@gmail.com>
2019-12-02 07:04:44 +0200

    Add generate docx word file with skeleton documentation

--- lib/inquisition/outputter/documentation.rb
+++ lib/inquisition/outputter/documentation.rb
@@ -1,4 +1,5 @@
-require 'puredocx'
+require_relative 'html/template'
+require_relative 'html/overview'
 
 module Inquisition
   module Outputter
@@ -8,21 +9,22 @@ module Inquisition
       end
 
       def generate_doc
-        PureDocx.create(Inquisition::Configuration.instance.output_path + define_file_name) do |doc|
-          doc.content([
-                        doc.text(Rails.root.to_s.split('/').last, size: 40, align: 'center')
-                      ])
-        end
+        documentation = HTML::Template.new('documentation_word').render(HTML::Overview.new(@collection))
+        File.open(create_folder_with_file, 'w') { |file| file.write(documentation) }
       end
 
       private
+
+      def create_folder_with_file
+        Configuration.instance.output_path + define_file_name
+      end
 
       def define_file_name
         "/#{project_file_name}_#{date_file_name}.docx"
       end
 
       def project_file_name
-        Rails.root.to_s.split('/').last.tr('-', '_')
+        Rails.application.class.parent_name
       end
 
       def date_file_name
