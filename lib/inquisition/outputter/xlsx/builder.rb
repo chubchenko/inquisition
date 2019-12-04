70499799d8ae7f620cfea68278a4dd2441bf5b20
Maxim <maxim.koptsov@rubygarage.org>
2019-12-04 12:25:16 +0200

    Added xlsx file generation

---
+++ lib/inquisition/outputter/xlsx/builder.rb
@@ -0,0 +1,36 @@
+require 'caxlsx'
+
+module Inquisition
+  module Outputter
+    class XLSX
+      class Builder
+        attr_reader :package
+
+        def self.call(collection)
+          new(collection).call
+        end
+
+        def initialize(collection)
+          @collection = collection
+          @package = Axlsx::Package.new
+        end
+
+        def call
+          package.serialize(path)
+        end
+
+        private
+
+        def path
+          File.join(
+            Inquisition::Configuration.instance.output_path, filename + '.xlsx'
+          )
+        end
+
+        def filename
+          [Rails.application.class.parent.name.underscore, Time.current.strftime('%d_%m_%y')].join('_')
+        end
+      end
+    end
+  end
+end
---
+++ spec/inquisition/outputter/xlsx/builder_spec.rb
@@ -0,0 +1,24 @@
+RSpec.describe Inquisition::Outputter::XLSX::Builder do
+
+  after { FileUtils.rm_rf(Inquisition::Configuration.instance.output_path) }
+
+  describe '.call' do
+    before do
+      allow(builder).to receive(:call)
+      allow(described_class).to receive(:new).and_return(builder)
+      described_class.call([])
+    end
+
+    let(:builder) { instance_double(described_class) }
+    it { expect(builder).to have_received(:call) }
+  end
+
+  describe '#call' do
+    before { described_class.call([]) }
+
+    it 'creates xlsx file' do
+      filename = "dummy_#{Time.current.strftime('%d_%m_%y')}"
+      expect(File).to exist(File.join(Inquisition::Configuration.instance.output_path, filename + '.xlsx'))
+    end
+  end
+end
